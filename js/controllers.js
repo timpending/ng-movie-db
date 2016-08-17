d4acda0382e5a0a0fdbfff2725b3e943427e3768
Tim Pender <mrtimpender@gmail.com>
2016-08-16 21:04:27 -0600

    get returning movies, styled

--- js/controllers.js
+++ js/controllers.js
@@ -1,3 +1,19 @@
-app.controller('SearchController', function($scope){
-  alert('working');
+app.controller('SearchController', function($scope, $http){
+  $scope.view = {};
+  $scope.view.error = null;
+
+  $scope.submitSearch = function() {
+    var movie = $scope.movieSearch.movie
+    console.log(movie);
+    console.log('******');
+    $http.get('http://www.omdbapi.com/?s='+movie).then(function successCallback(data){
+      console.log(data);
+      $scope.view.movies = data.data.Search
+    }, function errorCallback(response){
+      $scope.view.error = 'There was an error with the search.'
+    });
+
+  }
+
+
 })
